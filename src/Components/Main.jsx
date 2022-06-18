c616b71615e84cb3ef2c1426a1cf089b36191320
Nicolas <56944680+nicoc12024@users.noreply.github.com>
2022-06-18 13:08:36 +0200

    main

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 import "../stylesheets/Main.css";
 import axios from "axios";
-import moment from "moment";
 
 function Main() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
-  const [month, setMonth] = useState("");
-  const [day, setDay] = useState("");
   const [icon, setIcon] = useState("");
 
   const apiKey = "f1e7121e219070f4e629e794f41df9b7";
@@ -21,11 +18,6 @@ function Main() {
       .get(url)
       .then((response) => {
         const imgSrc = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`;
-        const dayDOM = moment().format("dddd");
-        const monthDOM = moment().format("LL");
-        setMonth(monthDOM);
-        setDay(dayDOM);
-
         setIcon(imgSrc);
         setData(response.data);
       })
@@ -38,28 +30,78 @@ function Main() {
 
   return (
     <div className="main">
-      <form className="form" onSubmit={searchLocation}>
-        <input
-          type="text"
-          name="city"
-          id="city"
-          placeholder="Search for location"
-          required
-          onChange={(e) => setLocation(e.target.value)}
-          value={location}
-        />
-        <button type="submit">SEARCH</button>
-      </form>
-      <div className="card">
-        <div className="infoTop">
-          <p className="day">{day}</p>
-          <p className="date">{month}</p>
-          <p className="location">{data.name}</p>
+      <div className="weather-content">
+        <div id="title">
+          <h2>Weather App</h2>
         </div>
-        <div className="infoBottom">
-          <div> {data.main ? <p>{data.main.temp.toFixed()} C</p> : null}</div>
-          <p className="weather"> {data.weather ? data.weather[0].main : null}</p>
-          {icon ? <img src={icon} alt="icon" /> : null}
+        <form className="form" onSubmit={searchLocation}>
+          <p className="info-text">Please enter a valid city name</p>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            placeholder="Enter city name"
+            spellCheck="false"
+            required
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+          />
+          <div className="separator"></div>
+          <button type="submit">Search</button>
+        </form>
+
+        {/* Weather Information */}
+        <div className="card">
+          <div className="infoTop">
+            {/* Temperature */}
+            <div className="temperature">
+              {data.main ? <p>{data.main.temp.toFixed()}° C</p> : null}
+            </div>
+
+            {/* Weather */}
+            <p className="weather"> {data.weather ? data.weather[0].main : null}</p>
+
+            {/* Icon */}
+            {icon ? <img src={icon} alt="icon" width={"50px"} /> : null}
+
+            {/* Location */}
+            <p className="location">
+              {data.main ? (
+                <p>
+                  <i className="fa-solid fa-location-dot"></i> {data.name}
+                </p>
+              ) : null}
+            </p>
+          </div>
+
+          {/* Info Bottom */}
+
+          {data.main ? (
+            <div className="infoBottom">
+              <div className="infoLeft">
+                <p className="feelsLike">
+                  {data.main ? (
+                    <p>
+                      <i className="fa-solid fa-temperature-half"></i>
+                      {data.main.feels_like.toFixed()}° C <br />{" "}
+                      <span className="textInfo">Feels like</span>
+                    </p>
+                  ) : null}
+                </p>
+              </div>
+              <div className="infoRight">
+                <p className="feelsLike">
+                  {data.main ? (
+                    <p>
+                      <i className="fa-solid fa-droplet"></i>{" "}
+                      {data.main.humidity.toFixed()}% <br />{" "}
+                      <span className="textInfo">Humidity</span>
+                    </p>
+                  ) : null}
+                </p>
+              </div>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "./Header";
 import Main from "./Main";
 
 function WeatherApp() {
   return (
     <>
-      <Header />
       <Main />
     </>
   );
